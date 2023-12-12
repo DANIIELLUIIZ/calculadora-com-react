30a0596b11b337d0d2daab553979b2bbe3be94ef
Daniel Freitas <danielluiz358@gmail.com>
2023-12-11 21:29:04 -0300

    calculadora usando React

---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #aaffaa;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+  font-family: Roboto;
+
+  input {
+    width: 100%;
+    height: 100%;
+    background-color: #FF0000;
+    border: 0;
+    outline: none;
+    text-align: right;
+    padding: 0 10px;
+    color: #ffffff;
+    font-size: 24px;
+    font-family: Roboto;
+  }
+`
